fe52710b62a06a0f329c93b408b2b7a8476b19ca
Carlos Gamuci <carlos.gamuci@gmail.com>
2022-10-14 09:42:54 +0200

    Actualización pom.xml
    - Se agregan las dependencias de BouncyCastle e iText al pom.xml, ya que se utilizaban pero hasta ahora sólo se importaban de forma indirecta.

--- Integra-parent/Integra-sign-operations/src/main/java/es/gob/afirma/utils/UtilsResourcesSignOperations.java
+++ Integra-parent/Integra-sign-operations/src/main/java/es/gob/afirma/utils/UtilsResourcesSignOperations.java
@@ -1,3 +1,15 @@
+//Copyright (C) 2012-13 MINHAP, Gobierno de España
+//This program is licensed and may be used, modified and redistributed under the terms
+//of the European Public License (EUPL), either version 1.1 or (at your
+//option) any later version as soon as they are approved by the European Commission.
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+//or implied. See the License for the specific language governing permissions and
+//more details.
+//You should have received a copy of the EUPL1.1 license
+//along with this program; if not, you may find it at
+//http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
 package es.gob.afirma.utils;
 
 import java.io.ByteArrayInputStream;
@@ -11,32 +23,13 @@ import org.apache.xml.security.c14n.implementations.Canonicalizer11_OmitComments
 import org.apache.xml.security.c14n.implementations.Canonicalizer11_WithComments;
 
 import es.gob.afirma.i18n.ILogConstantKeys;
-//Copyright (C) 2012-13 MINHAP, Gobierno de España
-//This program is licensed and may be used, modified and redistributed under the terms
-//of the European Public License (EUPL), either version 1.1 or (at your
-//option) any later version as soon as they are approved by the European Commission.
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS,
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-//or implied. See the License for the specific language governing permissions and
-//more details.
-//You should have received a copy of the EUPL1.1 license
-//along with this program; if not, you may find it at
-//http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
-/**
-* <b>File:</b><p>es.gob.afirma.utils.UtilsResources.java.</p>
-* <b>Description:</b><p>Class that provides functionality to control of resources from sign-operations module.</p>
-* <b>Project:</b><p>Library for the integration with the services of @Firma, eVisor and TS@.</p>
-* <b>Date:</b><p>13/01/2014.</p>
-* @author Gobierno de España.
-* @version 1.3, 18/04/2022.
-*/
 import es.gob.afirma.i18n.Language;
 import es.gob.afirma.logger.Logger;
 
 /**
  * <p>Class that provides functionality to control the resources for sign-operations module.</p>
  * <b>Project:</b><p>Library for the integration with the services of @Firma, eVisor and TS@.</p>
+ * @author Gobierno de España.
  * @version 1.3, 18/04/2022.
  */
 public class UtilsResourcesSignOperations {
